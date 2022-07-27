1e29ce7a5e35a3ea9843e051301dab3c36b57575
DreamOneX <me@dreamonex.eu.org>
2022-07-27 23:56:03 +0800

    style: code format

--- src/aipg/aipg.py
+++ src/aipg/aipg.py
@@ -1,5 +1,12 @@
-import requests, json, base64
+'''
+authlib-injector parameter generator
+'''
+import json
+import base64
+import requests
+
 __all__ = ["SkinWebsite", "Account"]
+
 
 class LoginError(Exception):
     '''
@@ -7,14 +14,17 @@ class LoginError(Exception):
     username not found
     etc
     '''
+
     def __init__(self, message: str = 'Your username or password is invalid') -> None:
         super().__init__(message)
         self.message = message
+
 
 class SkinWebsite():
     '''
     A yggdrasil api site like Blessing Skin
     '''
+
     def __init__(self, api_base: str) -> None:
         '''
         api_base Yggdrasil Api URL like
@@ -29,10 +39,12 @@ class SkinWebsite():
         '''
         return base64.b64encode(requests.get(self.api_base).text.encode('ascii')).decode('ascii')
 
+
 class Account():
     '''
     An yggdrasil account
     '''
+
     def __init__(self, website: SkinWebsite, **kwargs) -> None:
         '''
         website website object
@@ -40,17 +52,23 @@ class Account():
         password ¿dont you know
         '''
         self.website = website
-        if kwargs.get('username') != None: self.__username = str(kwargs.get('username'))
-        else: self.__username = None
-        if kwargs.get('password') != None: self.__password = str(kwargs.get('password'))
-        else: self.__password = None
+        if kwargs.get('username') is not None:
+            self.__username = str(kwargs.get('username'))
+        else:
+            self.__username = None
+        if kwargs.get('password') is not None:
+            self.__password = str(kwargs.get('password'))
+        else:
+            self.__password = None
         self.__header = {'Content-Type': 'application/json'}
+        self.access_token = None
+        self.client_token = None
 
     def __build_data(self, username: str, password: str) -> str:
         return json.dumps({'username': username, 'password': password, 'requestUser': False, 'agent': {'name': 'Minecraft', 'version': 1}})
 
     def __check_info(self) -> bool:
-        if (self.__username == None or self.__password == None):
+        if (self.__username is None or self.__password is None):
             raise LoginError("No email or password provided")
         if ((not isinstance(self.__username, str)) or (not isinstance(self.__password, str))):
             raise LoginError("Email or password is not a instance of str")
@@ -61,17 +79,19 @@ class Account():
         username email or id
         if you provide at instantiation, no need provide again
         '''
-        if kwargs.get('username') != None: self.__username = str(kwargs.get('username'))
-        if kwargs.get('password') != None: self.__password = str(kwargs.get('password'))
+        if kwargs.get('username') is not None:
+            self.__username = str(kwargs.get('username'))
+        if kwargs.get('password') is not None:
+            self.__password = str(kwargs.get('password'))
         self.__check_info()
-        response = requests.post(\
-                self.website.api_base + "/authserver/authenticate", \
-                headers=self.__header, \
-                data=self.__build_data(\
-                    self.__username, \
-                    self.__password\
-                ) \
+        response = requests.post(
+            self.website.api_base + "/authserver/authenticate",
+            headers=self.__header,
+            data=self.__build_data(
+                self.__username,
+                self.__password
             )
+        )
         body = json.loads(response.text)
         if response.status_code != 200:
             raise LoginError
